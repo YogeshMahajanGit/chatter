8108832268c2e8fcfcf14378eed9a40c97d58b27
Your_Name <Your_Email>
2024-11-18 15:50:57 +0530

    add code

---
+++ client/src/helpers/uploadFile.js
@@ -0,0 +1,20 @@
+// https://api.cloudinary.com/v1_1/demo/image/upload
+
+//upload url
+const URL = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/auto/upload`;
+
+const uploadFile = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "chat-app-file");
+
+  const response = await fetch(URL, {
+    method: "post",
+    body: formData,
+  });
+  const responseData = await response.json();
+
+  return responseData;
+};
+
+export default uploadFile;
